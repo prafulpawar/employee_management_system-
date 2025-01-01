45b1bccd980264ba8ba01354ef09c6eec4979c31
prafulpawar <prafuldukhi@gmail.com>
2025-01-01 23:59:18 +0530

    "Added"

---
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Header from '../Other/Header'
+
+function AdminDashboard() {
+  return (
+    <div className='h-screen w-full p-10'>
+         <Header/>
+
+         <div>
+            <form className='flex items-center justify-between ' >
+
+                 <div>
+                        <h3>Task Title</h3>
+                        <input type="text" placeholder='Make A UI Design' />
+                 </div>
+
+                 <div>
+                         <h3>Description</h3>
+                         <textarea name="" cols="30" rows="10" ></textarea>
+                 </div>
+
+                 <div>
+                         <h3>Date</h3>
+                         <input type="date" />
+                 </div>
+
+                 <div>
+                          <h3>Asign To</h3>
+                          <input type="text" placeholder='Employee Name' />
+                 </div>
+                 
+                 <div>
+                             <h3>Category</h3>
+                             <input type="text" placeholder='design,dev,etc' />
+                 </div>
+
+                 <button>
+                       Create Task
+                 </button>
+                
+            </form>
+         </div>
+         
+    </div>
+  )
+}
+
+export default AdminDashboard
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
 
 function TaskList() {
-  return (
-    <div className='h-[55%] w-full bg-red-500 mt-10'>
-          
-    </div>
-  )
+    return (
+
+        <div id='tasklist'  className='h-[55%] overflow-x-auto flex gap-5 justify-start items-center flex-nowrap  w-full py-5  mt-10'>
+           
+            <div className='h-full flex-shrink-0 w-[300px] bg-red-400 rounded-xl p-5 '>
+                 <div className='flex  justify-between items-center'>
+                     <h3 className='bg-red-600 px-3 text-sm py-1 rounded'>  High </h3>
+                     <h4 className='text-sm'>20 feb 2024</h4>
+                 </div>
+                 <h2 className='mt-5 text-2xl font-semibold'> Make A Youtube Vedio </h2>
+                 <p className='text-sm mt-2 '>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quidem natus officiis quisquam eveniet cumque.
+                 </p>
+            </div>
+
+            <div className='h-full flex-shrink-0 w-[300px] bg-green-400 rounded-xl p-5 '>
+                 <div className='flex  justify-between items-center'>
+                     <h3 className='bg-red-600 px-3 text-sm py-1 rounded'>  High </h3>
+                     <h4 className='text-sm'>20 feb 2024</h4>
+                 </div>
+                 <h2 className='mt-5 text-2xl font-semibold'> Make A Youtube Vedio </h2>
+                 <p className='text-sm mt-2 '>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quidem natus officiis quisquam eveniet cumque.
+                 </p>
+            </div>
+
+            <div className='h-full flex-shrink-0 w-[300px] bg-blue-400 rounded-xl p-5 '>
+                 <div className='flex  justify-between items-center'>
+                     <h3 className='bg-red-600 px-3 text-sm py-1 rounded'>  High </h3>
+                     <h4 className='text-sm'>20 feb 2024</h4>
+                 </div>
+                 <h2 className='mt-5 text-2xl font-semibold'> Make A Youtube Vedio </h2>
+                 <p className='text-sm mt-2 '>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quidem natus officiis quisquam eveniet cumque.
+                 </p>
+            </div>
+
+            <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl p-5 '>
+                 <div className='flex  justify-between items-center'>
+                     <h3 className='bg-red-600 px-3 text-sm py-1 rounded'>  High </h3>
+                     <h4 className='text-sm'>20 feb 2024</h4>
+                 </div>
+                 <h2 className='mt-5 text-2xl font-semibold'> Make A Youtube Vedio </h2>
+                 <p className='text-sm mt-2 '>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quidem natus officiis quisquam eveniet cumque.
+                 </p>
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default TaskList
